9f270a81a7a8c354f3d49f91d91f18f68133cb6d
Amrnash <amrnashaat8@gmail.com>
2023-08-23 15:31:27 +0300

    Add script to set vendorId on apiVendors

---
+++ set-vendorId-on-apivendor/index.js
@@ -0,0 +1,43 @@
+import { MongoClient } from "mongodb";
+
+const devURI =
+  "mongodb+srv://devdb:UDBoHHLHrhjvyMnY@botit-dev.jwtve.mongodb.net/botitdev?retryWrites=true&w=majority";
+const client = new MongoClient(devURI);
+
+const apiVendorCol = client.db("botitdev").collection("ApiVendors");
+const vendorCol = client.db("botitdev").collection("Vendors");
+
+function getApiVendorsWithNoVendorId() {
+  return apiVendorCol.find({ "info.vendorId": { $exists: false } }).toArray();
+}
+
+async function getVendorIdOfApiVendor(apiVendor) {
+  const regex = new RegExp(apiVendor.vendor, "i");
+  const vendor = await vendorCol.findOne({ "name.en": regex });
+  return vendor ? vendor._id : null;
+}
+
+const apiVendors = await getApiVendorsWithNoVendorId();
+const updates = [];
+
+for (const apiVendor of apiVendors) {
+  const vendorId = await getVendorIdOfApiVendor(apiVendor);
+  if (!vendorId) continue;
+  updates.push({
+    updateOne: {
+      filter: { vendor: apiVendor.vendor },
+      update: {
+        $set: {
+          "info.vendorId": vendorId,
+        },
+      },
+    },
+  });
+  try {
+    console.log("Writing to Database...");
+    await apiVendorCol.bulkWrite(updates);
+    client.close();
+  } catch (error) {
+    console.error(error);
+  }
+}
